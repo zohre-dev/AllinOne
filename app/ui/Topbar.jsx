9e21cb62199c576606ab8664eb8cb120edbab313
zohre-dev <mehrabi.z13@gmail.com>
2023-12-07 06:50:19 +0200

    build:created Topbar component

---
+++ app/ui/Topbar.jsx
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+  faSquarePinterest,
+  faTwitter,
+  faSquareFacebook,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
+import Image from 'next/image'
+
+const Topbar = () => {
+  return (
+    <div className="sticky top-0 flex h-[50px] w-full items-center">
+      <div className="topRight  flex-[3] ">
+        <Image
+          src="/images/profile.jpg"
+          width={40}
+          height={40}
+          alt="profile image"
+          className="cursor-pointer rounded-full object-cover"
+        />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className="text-lg text-[#666]"
+        />
+      </div>
+
+      <ul className="topCenter flex-[6] ">
+        <li>خانه</li>
+        <li>اساتید</li>
+        <li>درباره ی ما</li>
+        <li>خروج</li>
+      </ul>
+
+      <div className="topLeft flex-[3] cursor-pointer text-[#444]">
+        <FontAwesomeIcon icon={faTwitter} />
+        <FontAwesomeIcon icon={faSquareFacebook} />
+        <FontAwesomeIcon icon={faSquarePinterest} />
+        <FontAwesomeIcon icon={faInstagram} />
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
