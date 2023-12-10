70741d3cb2d067a39a58ad961fa4e703c3d4d9e9
zohre-dev <mehrabi.z13@gmail.com>
2023-12-10 10:14:43 +0200

    chore:changed some style

---
+++ app/ui/Search.jsx
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+const Search = () => {
+  return (
+    <div className="mذ-[15px] flex items-center  gap-[10px] bg-[#2e374a] p-4">
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        className="text-lg text-[#7e7d7d]"
+      />
+      <input
+        type="text"
+        placeholder="جستجو"
+        className="w-[50%] p-2 text-[#7e7d7d] outline-none"
+      />
+    </div>
+  )
+}
+
+export default Search
