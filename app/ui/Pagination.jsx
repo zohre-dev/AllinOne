e8c1268e4dc26b8ff989e9f7e8ca022cd8d842ee
zohre-dev <mehrabi.z13@gmail.com>
2023-12-10 08:05:21 +0200

    build:created pagination component

---
+++ app/ui/Pagination.jsx
@@ -0,0 +1,14 @@
+const Pagination = () => {
+  return (
+    <div className="mt-[15px]  flex items-center justify-between">
+      <button className="max-w-fit cursor-pointer border-none bg-[#ff5555] px-6 py-3 text-white">
+        بعدی
+      </button>
+      <button className="max-w-fit cursor-pointer border-none bg-[#ff5555] px-6 py-3 text-white">
+        قبلی
+      </button>
+    </div>
+  )
+}
+
+export default Pagination
